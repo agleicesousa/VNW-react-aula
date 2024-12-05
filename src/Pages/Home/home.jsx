90d4c355247d98852938f743d4a8c7da3878e3a4
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-12-04 22:39:00 -0300

    feat: importações home

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,3 +1,8 @@
+import s from './home.module.scss'
+import imagem1 from '../../assets/imagem1doacao.png'
+import imagem2 from '../../assets/imagem2doacao.png'
+import imagem3 from '../../assets/imagem3doacao.png'
+import imagem4 from '../../assets/imagem4doacao.png'
 
 export default function Home() {
   return (
