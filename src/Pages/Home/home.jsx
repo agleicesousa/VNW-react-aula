52288fafdcb118b845294375530733eef89ab0ae
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-12-04 23:06:32 -0300

    feat: tag vazia

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -6,50 +6,52 @@ import imagem4 from "../../assets/imagem4doacao.png";
 
 export default function Home() {
   return (
-    <main>
-      <section className={s.primeiraSessaoInicio}>
-        <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
-      </section>
-      <section className={s.porqueDoarSection}>
-        <h2>Por que devo doar?</h2>
-        <div className={s.containerCards}>
-          <section>
-            <img
-              src={imagem1}
-              alt="Imagem ilustrando círculo social de 4 pessoas"
-            />
-            <p>
-              {" "}
-              Oferece livros a quem não tem acesso, ajudando a reduzir a
-              exclusão social.
-            </p>
-          </section>
-          <section>
-            <img src={imagem2} alt="Imagem ilustrando pessoa lendo" />
-            <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
-          </section>
-          <section>
-            <img
-              src={imagem3}
-              alt="Imagem ilustrando 6 pessoas transformando o mundo"
-            />
-            <p>
-              Fornece conhecimento e inspiração, permitindo que indivíduos
-              transformem suas vidas.
-            </p>
-          </section>
-          <section>
-            <img
-              src={imagem4}
-              alt="Imagem ilustrando balança que representa igualdade"
-            />
-            <p>
-              Garante que todos, independentemente de sua condição, tenham
-              oportunidades de aprendizado.
-            </p>
-          </section>
-        </div>
-      </section>
-    </main>
+    <>
+      <main>
+        <section className={s.primeiraSessaoInicio}>
+          <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+        </section>
+        <section className={s.porqueDoarSection}>
+          <h2>Por que devo doar?</h2>
+          <div className={s.containerCards}>
+            <section>
+              <img
+                src={imagem1}
+                alt="Imagem ilustrando círculo social de 4 pessoas"
+              />
+              <p>
+                {" "}
+                Oferece livros a quem não tem acesso, ajudando a reduzir a
+                exclusão social.
+              </p>
+            </section>
+            <section>
+              <img src={imagem2} alt="Imagem ilustrando pessoa lendo" />
+              <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+            </section>
+            <section>
+              <img
+                src={imagem3}
+                alt="Imagem ilustrando 6 pessoas transformando o mundo"
+              />
+              <p>
+                Fornece conhecimento e inspiração, permitindo que indivíduos
+                transformem suas vidas.
+              </p>
+            </section>
+            <section>
+              <img
+                src={imagem4}
+                alt="Imagem ilustrando balança que representa igualdade"
+              />
+              <p>
+                Garante que todos, independentemente de sua condição, tenham
+                oportunidades de aprendizado.
+              </p>
+            </section>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
