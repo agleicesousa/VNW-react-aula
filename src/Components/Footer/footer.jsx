0396eded3dee935334f0ecf5e53ee1dc55bf7576
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-12-04 22:47:08 -0300

    feat: importações - footer

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,9 +1,14 @@
+import logoFacebook from "../../assets/logoFacebook.png";
+import logoTwitter from "../../assets/logoTwitter.png";
+import logoYoutube from "../../assets/logoYoutube.png";
+import logoLinkedin from "../../assets/logoLinkedin.png";
+import logoInstagram from "../../assets/logoInstagram.png";
+import s from "./footer.module.scss";
+
 export default function Footer() {
-    return (
-        <>
-            <footer>
-                <p>É o FOOTER</p>
-            </footer>
-        </>
-    );
+  return (
+    <footer>
+      <p>É o FOOTER</p>
+    </footer>
+  );
 }
