c38bc54997678744f5582ff8ff8f2ce14a96db51
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-12-04 22:43:38 -0300

    refactor: formatação prettier + html header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,32 +1,50 @@
-
-import { Link } from 'react-router-dom';
-import logoLivro from '../../assets/logoLivro.png';
-import lupa from '../../assets/lupa.png';
-
-import s from './header.module.scss';
+import { Link } from "react-router-dom";
+import logoLivro from "../../assets/logoLivro.png";
+import lupa from "../../assets/lupaHeader.png";
+import s from "./header.module.scss";
 
 export default function Header() {
-    return (
-        <>
-            <header className={s.header} >
-                <section className={s.logoHeader} >
-                    <img src={logoLivro} alt='Imagem de um livro branco com fundo azul' />
-                    <h1>Livro Vai Na Web</h1>
-                </section>
-                <nav className={s.navHeader} >
-                    <ul>
-                        <li><Link className={s.link} to='/'>Inicio</Link></li>
-                        <li><Link className={s.link} to='/livros-doados'>Livros Doados</Link></li>
-                        <li><Link className={s.link} to='/quero-doar'>Quero doar</Link></li>
-                    </ul>
-                </nav>
-                <section className={s.barraDeBusca} >
-                    <input type='search' name='' id='' placeholder='O que você procura?' />
-                    <button>
-                        <img src={lupa} alt='Ícone de lupa' />
-                    </button>
-                </section>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <section className={s.logoHeader}>
+          <img
+            src={logoLivro}
+            alt="Imagem de uma ilustração de um livro aberto com capa azul"
+          />
+          <h1>Livros Vai na Web</h1>
+        </section>
+        <nav className={s.navHeader}>
+          <ul>
+            <li>
+              <Link className={s.link} to="/">
+                Início
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} to="/livrosdoados">
+                Livros Doados
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} to="/querodoar">
+                Quero Doar
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <section className={s.barraDeBusca}>
+          <input
+            type="search"
+            name=""
+            id=""
+            placeholder="O que você procura?"
+          />
+          <button>
+            <img src={lupa} alt="Imagem deu uma lupa branca" />
+          </button>
+        </section>
+      </header>
+    </>
+  );
 }
