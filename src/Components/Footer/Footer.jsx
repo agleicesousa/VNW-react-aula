d762880628aff2664ce3a73b6ae3a4a6ba4424a9
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-29 07:38:30 -0300

    style: substituir logos de redes sociais por ícones do react-icons e melhorar a estrutura do footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,35 +1,41 @@
-import logoFacebook from "../../assets/logoFacebook.png";
-import logoTwitter from "../../assets/logoTwitter.png";
-import logoYoutube from "../../assets/logoYoutube.png";
-import logoLinkedin from "../../assets/logoLinkedin.png";
-import logoInstagram from "../../assets/logoInstagram.png";
+import { FaFacebook, FaTwitter, FaYoutube, FaLinkedin, FaInstagram } from "react-icons/fa";
 import s from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={s.footer}>
       <section className={s.contatosFooter}>
-        <p>4002-8922</p>
-        <nav>
-          <a href="#">
-            <img src={logoFacebook} alt="Logo do Facebook" />
-          </a>
-          <a href="#">
-            <img src={logoTwitter} alt="Logo do Twitter" />
-          </a>
-          <a href="#">
-            <img src={logoYoutube} alt="Logo do Youtube" />
-          </a>
-          <a href="#">
-            <img src={logoLinkedin} alt="Logo do Linkedin" />
-          </a>
-          <a href="#">
-            <img src={logoInstagram} alt="Logo do Instagram" />
-          </a>
-        </nav>
+        <p className={s.phone}>4002-8922</p>
+        <ul className={s.socialLinks}>
+          <li>
+            <a href="#" aria-label="Facebook">
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="Twitter">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="Youtube">
+              <FaYoutube />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="Linkedin">
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="Instagram">
+              <FaInstagram />
+            </a>
+          </li>
+        </ul>
       </section>
       <section className={s.copyright}>
-        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024 </p>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
       </section>
     </footer>
   );
