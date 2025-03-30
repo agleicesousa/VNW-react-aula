7f140a8b9d5ef077c94f929c3fb7a2f28d7582a2
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-30 02:01:58 -0300

    chore: adicionar arquivo app.js com configuração inicial da API

---
+++ src/services/app.js
@@ -0,0 +1,8 @@
+//* Ainda não está sendo utilizado.
+
+// // Configura a base URL da API
+// const api = import.meta.env.REACT_APP_API_BASE_URL
+
+// // Endpoints (agora centralizados e configuráveis)
+// const API_ENDPOINTS = import.meta.env.REACT_APP_API_LIVROS_ENDPOINT
+
