462f4e120e252f9b26f8f2931f76ace7e8491fbb
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-30 02:11:51 -0300

    feat: implementar serviço de criação de livros utilizando axios

--- src/services/app.js
+++ src/services/app.js
@@ -1,8 +1,21 @@
-//* Ainda não está sendo utilizado.
+import axios from 'axios';
 
-// // Configura a base URL da API
-// const api = import.meta.env.REACT_APP_API_BASE_URL
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL
+});
 
-// // Endpoints (agora centralizados e configuráveis)
-// const API_ENDPOINTS = import.meta.env.REACT_APP_API_LIVROS_ENDPOINT
+const API_ENDPOINTS = {
+    LIVROS: import.meta.env.VITE_API_LIVROS_ENDPOINT
+};
 
+export const livroService = {
+    create: async (livroData) => {
+        try {
+            const response = await api.post(API_ENDPOINTS.LIVROS, [livroData]);
+            return response.data;
+        } catch (error) {
+            console.error('Erro ao cadastrar livro:', error.response?.data || error.message);
+            throw error;
+        }
+    },
+};
