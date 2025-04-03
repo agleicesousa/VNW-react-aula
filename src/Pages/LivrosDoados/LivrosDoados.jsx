a159ee13f1be3115027de38e92a76e678ffffbc4
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-04-03 00:25:50 -0300

    refactor: ajustar formatação do código e remover imports não utilizados

--- src/Pages/LivrosDoados/LivrosDoados.jsx
+++ src/Pages/LivrosDoados/LivrosDoados.jsx
@@ -1,14 +1,20 @@
+// import { useState, useEffect } from "react";
 import livroProtagonista from "../../assets/livroProtagonista.png";
 import s from "./livrosDoados.module.scss";
+// import { livroService } from "../../services/app";
 
 export default function LivrosDoados() {
   return (
     <>
       <section className={s.livrosDoadosSection}>
         <h2>Livros Doados</h2>
+
         <section className={s.containerCards}>
           <section>
-            <img src={livroProtagonista} alt="Imagem do livro O Protagonista" />
+            <img
+              src={livroProtagonista}
+              alt="Imagem do livro O Protagonista"
+            />
             <div>
               <h3>O protagonista</h3>
               <p>Susanne Andrade</p>
