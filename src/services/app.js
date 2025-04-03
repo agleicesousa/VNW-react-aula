3a1d7727d4bee88521acd6fb8f75c96ae710ab17
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-04-03 08:34:45 -0300

    feat: adicionar função de busca ao serviço de livros com tratamento de erro

--- src/services/app.js
+++ src/services/app.js
@@ -1,3 +1,4 @@
+// src/services/app.js
 import axios from "axios";
 
 const api = axios.create({
@@ -8,7 +9,7 @@ export const livroService = {
     create: async (livroData) => {
         try {
             const response = await api.post(
-                import.meta.env.VITE_API_LIVROS_ENDPOINT, // Endpoint dinâmico
+                import.meta.env.VITE_API_LIVROS_ENDPOINT,
                 [livroData]
             );
             return response.data;
@@ -34,6 +35,20 @@ export const livroService = {
                 data: error.response?.data,
                 endpoint: error.config?.url,
             })
+            throw error;
+        }
+    },
+
+    search: async (query) => {
+        try {
+            const response = await api.get(`${import.meta.env.VITE_API_LIVROS_ENDPOINT}/buscar?q=${query}`);
+            return response.data;
+        } catch (error) {
+            console.error("Erro na busca:", {
+                status: error.response?.status,
+                data: error.response?.data,
+                endpoint: error.config?.url,
+            });
             throw error;
         }
     }
